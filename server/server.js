8ccabd9289eb7363ace87c7b0fb32a480507d81d
karanshah254 <karankshah2504@gmail.com>
2024-07-29 16:40:59 +0530

    installed dotenv file and managed connection string

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB Atlas
-const dbURI = "your mongodb url";
+const dbURI = process.env.MONGO_URL;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Define Note model
